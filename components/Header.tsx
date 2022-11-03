6727eb877ca18a147fee860dbbf6f158e0ef3907
Elias Vedovato <eliasvedovato@gmail.com>
2022-11-02 21:11:37 -0300

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { SearchIcon, ShoppingBagIcon, UserIcon } from '@heroicons/react/outline'
+
+function Header() {
+	const session = false
+
+	return (
+		<header className='sticky top-0 z-30 flex w-full items-center justify-between bg-[#e7ecee] p-4'>
+			<div className='flex items-center justify-center md:w-1/5'>
+				<Link href='/'>
+					<div
+						className='relative h-10 w-5 cursor-pointer
+            			opacity-75 transition hover:opacity-100'
+					>
+						<Image
+							src='https://rb.gy/vsvv2o'
+							width='200'
+							height='200'
+							layout='fill'
+							objectFit='contain'
+						/>
+					</div>
+				</Link>
+			</div>
+			<div className='hidden flex-1 items-center justify-center space-x-8 md:flex'>
+				<a className='headerLink'>Product</a>
+				<a className='headerLink'>Explore</a>
+				<a className='headerLink'>Support</a>
+				<a className='headerLink'>Business</a>
+			</div>
+			<div className='flex items-center justify-center gap-x-4 md:w-1/5'>
+				<SearchIcon className='headerIcon' />
+				<Link href='/checkout'>
+					<div className='relative cursor-pointer'>
+						<span
+							className='absolute -right-1 -top-1 z-50 flex w-4 items-center 
+						justify-center rounded-full bg-gradient-to-r from-pink-500 
+						to-violet-500 text-[10px] text-white'
+						>
+							5
+						</span>
+						<ShoppingBagIcon className='headerIcon' />
+					</div>
+				</Link>
+
+				{session ? (
+					<Image
+						src={
+							session.user?.image ||
+							'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
+						}
+						alt=''
+						className='cursor-pointer rounded-full'
+						width={34}
+						height={34}
+						onClick={() => signOut()}
+					/>
+				) : (
+					<UserIcon className='headerIcon' onClick={() => signIn()} />
+				)}
+			</div>
+		</header>
+	)
+}
+
+export default Header
